99671cb822fd0c593bee75b2dc50b4d291464655
Anurag Pradhan <123073956+anurag6372@users.noreply.github.com>
2023-07-08 22:52:00 +0530

    Services and Repo Updated

--- Backend/src/main/java/com/example/demo/service/RecruiterService.java
+++ Backend/src/main/java/com/example/demo/service/RecruiterService.java
@@ -1,22 +1,37 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.JobSeekerRepository;
+import com.example.demo.dao.RecruiterRepository;
+import com.example.demo.entity.JobSeeker;
 import com.example.demo.entity.Recruiter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RecruiterService {
 
-	public String register(Recruiter recruiter) {
-		// TODO Auto-generated method stub
-		return null;
+	@Autowired
+	RecruiterRepository repo;
+
+	public Recruiter register(Recruiter recruiter) throws Exception {
+		Recruiter tempObj = repo.findByEmail(recruiter.getEmail());
+		if(tempObj!=null) {
+			throw new Exception("This user already exists ");
+		}
+		return repo.save(recruiter);
 	}
 
-	public Recruiter loginJobSeeker() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Recruiter loginRecruiter(Recruiter recruiter) throws Exception {
+		Recruiter tempObj = repo.findByEmail(recruiter.getEmail());
+		if(tempObj==null) {
+			throw new Exception("User didn't exists");
+		}
+//		else if(!user.getPassword().equals(tempObj.getPassword())) {
+		else if(!recruiter.getPassword().equals(tempObj.getPassword())) {
+			throw new Exception("Bad credentials");
+		}
 
-	public Recruiter loginRecruiter(Recruiter recruiter) {
-		// TODO Auto-generated method stub
-		return null;
+		return tempObj;
 	}
 
 }
