40ec7bf8861d3c1cc640fecedad196ba9a1ec353
Anurag Pradhan <123073956+anurag6372@users.noreply.github.com>
2023-07-15 12:33:19 +0530

    login and registration form complete

--- client/src/app/Components/js-register/js-register.component.ts
+++ client/src/app/Components/js-register/js-register.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./js-register.component.css']
 })
 export class JSRegisterComponent {
+  displayVal:string[]=[];
+  display:boolean=(Array.isArray(this.displayVal) && this.displayVal.length >0);
+  getSkill(val:any){
+    if (val.length>0) {
+      
+      this.displayVal.push(val);
+      val ='';
+      this.display=(Array.isArray(this.displayVal) && this.displayVal.length >0);
+    }
+    
+    // console.warn(val);
+    // console.log(this.displayVal)
+    // this.displayVal = val;
+  }
+  deleteSkill(index:any){
+    this.displayVal.splice(index,1) 
+    this.display=(Array.isArray(this.displayVal) && this.displayVal.length >0); 
+    // console.log(index)
+  }
 
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './Components/login/login.component';
 import { RRegisterComponent } from './Components/r-register/r-register.component';
 import { JSRegisterComponent } from './Components/js-register/js-register.component';
 import { HomeComponent } from './Components/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HomeComponent } from './Components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
