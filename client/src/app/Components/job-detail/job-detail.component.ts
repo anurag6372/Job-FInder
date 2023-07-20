b82a3ecfa086b274f4b92bf18f64cdd97f9dd2a9
Chandresh Carpenter <123073214+Chandreshc@users.noreply.github.com>
2023-07-20 10:52:13 +0530

    changed little UI

--- client/src/app/Components/job-detail/job-detail.component.ts
+++ client/src/app/Components/job-detail/job-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-job-detail',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./job-detail.component.css']
 })
 export class JobDetailComponent {
+  constructor(private _route:Router){}
+routeBack() {
+  this._route.navigate(['/']);
+}
 
 }
